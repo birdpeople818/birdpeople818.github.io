fe1a0fe07dd9017c03c93bba0f194f9a8898e9b4
birdpeople818 <birdpeople818@gmail.com>
2023-05-04 00:45:38 +0000

    worked on runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -19,11 +19,13 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
                 { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "reward", "x": 2000, "y": groundY - 50 },
+                { "type": "enemy", "x": 400, "y": groundY - 45 },
             ]
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
@@ -42,9 +44,53 @@ var level01 = function (window) {
         createSawBlade(400, 380);
         createSawBlade(800, 470);
         createSawBlade(1000, 380);
-        
-        
-        
+
+        function createEnemy(x, y) {
+            var enemy = game.createGameItem("enemy", 25);
+            var greenSquare = draw.rect(50, 50, "green");
+            greenSquare.x = -25;
+            greenSquare.y = -25;
+            enemy.addChild(greenSquare);
+            enemy.x = x;
+            enemy.y = y;
+            game.addGameItem(enemy);
+            enemy.velocityX = -1;
+            enemy.rotationalVelocity = 100;
+            enemy.onPlayerCollision = function() {
+                game.changeIntegrity(-10)
+            };
+            enemy.onProjectileCollision = function(){
+                game.increaseScore(100);
+                enemy.fadeOut();
+            };
+        }
+        createEnemy(400, groundY - 45);
+        createEnemy(1000, groundY - 45);
+        createEnemy(1600, groundY - 45);
+
+        function createReward(x, y) {
+            var reward = game.createGameItem("reward", 50)
+            var purpleSquare = draw.rect(50, 50, "purple")
+            purpleSquare.x = -25;
+            purpleSquare.y = -25;
+            reward.addChild(purpleSquare);
+            reward.x = x;
+            reward.y = y;
+            game.addGameItem(reward);
+            reward.velocityX = -2;
+            reward.rotationalVelocity = 5;
+            reward.onPlayerCollision = function() {
+                game.changeIntegrity(+10000);
+                reward.fadeOut();
+            };
+            reward.onProjectileCollision = function() {
+                reward.fadeOut();
+            };
+
+        }
+        createReward(2000, groundY - 50)
+
+
         // DO NOT EDIT CODE BELOW HERE
     }
 };
